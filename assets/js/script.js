87ddcd2b61c1385f667497b7b549e5b7cf279be1
philip nguyen <nguyen.is.philip@gmail.com>
2023-11-28 14:27:03 -0500

    fixed alert to be a modal, added banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
-function submitSelection() {
     // BLESSING, please use this to implement the selected options 
-    const checkboxes = document.querySelectorAll('.retailer-checkbox:checked');
-    const selectedRetailers = Array.from(checkboxes).map(checkbox => checkbox.value);
-    alert('Selected Retailers: ' + selectedRetailers.join(', '));
+    function submitSelection() {
+        const checkboxes = document.querySelectorAll('.retailer-checkbox:checked');
+        const selectedRetailers = Array.from(checkboxes).map(checkbox => checkbox.value);
+    
+        const modalBody = document.getElementById('selectedRetailersBody');
+        
+        if (selectedRetailers.length === 0) {
+          modalBody.innerHTML = '<p>No retailers selected</p>';
+          // Show the modal
+          const modal = new bootstrap.Modal(document.getElementById('selectedRetailersModal'));
+          modal.show();
+        }
   }
