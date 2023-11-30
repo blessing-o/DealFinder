d42000a7e90a6d8563c986f1afb9de5c873ab135
blessing-o <oghaleokeme@gmail.com>
2023-11-30 18:36:27 -0500

    Fixed the checkbox

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,84 +1,119 @@
-// Function to redirect to the retailer's site
-function redirectToRetailer(retailerUrl, productId) {
-    window.location.href = retailerUrl + '?product_id=' + productId;
+/** Function to fetch product details and update the card
+ * @param product// the data gotten from the api is fetched
+ * @param filter// the site to visit
+ */
+function updateCard(product, filter) {
+  //dummy api
+  let products = [];
+  console.log(product);
+
+  for (i = 0; i < product.length; i++) {
+    products.push(product[i]);
+  }
+
+  console.log(products);
+
+  if (filter == "GoodDeal") {
+    displayProductsFakeStore(products);
+  } else {
+    displayProductsPlatzi(products);
+  }
 }
 
-// Function to fetch product details and update the card
-function updateCard(product) {
-    //dummy api
-    let products = [];
-    console.log(product);
-
-    for( i=0;i<product.length ;i++){
-        products.push(product[i]);
-    }
-    console.log(products);
-    displayProducts(products);
-}
-
-// Function to get URL parameter by name
-function getUrlParameter(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-}
-
-// Retrieve the selected item type from the URL
-const itemType = getUrlParameter('itemType');
-
-// Fetch from the apis. This is only one. 
-let ebayUrl = "https://cors-anywhere.herokuapp.com/https://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SECURITY-APPNAME=Blessing-DealFind-SBX-1f4a4f9a2-bc4924dd&RESPONSE-DATA-FORMAT=JSON&paginationInput.entriesPerPage=6&keywords=Monitors";
-
-let getFilter =function(){
-    // let searchParamsArr = document.location.search.split('&');
-
-  // Get the query and format values
-//   var query = searchParamsArr[0].split('=').pop();
-  let filter = document.location.search.split('=').pop();
+/**
+ * This function fetches the api depending on the site selected from the
+ * dropdown by the user
+ */
+let getFilter = function () {
+  let filter = document.location.search.split("=").pop();
   console.log(document.location.search);
   console.log(filter);
 
-  searchApi(filter);
-}
+  if (filter == "Platzi") {
+    fetch("https://api.escuelajs.co/api/v1/products/")
+      .then((response) => response.json())
+      .then((data) => updateCard(data))
+      .catch((error) =>
+        console.error("Error fetching product details:", error)
+      );
+  }
 
-let searchApi = function(filter){
-    console.log("i am in the search api function "+filter);
-    
-    let dummy = "https://api.escuelajs.co/api/v1/products/?title="+filter;
-fetch(dummy) // REPLACE THIS WITH THE URL
-    .then(response => response.json())
-    .then(data => updateCard(data))
-    .catch(error => console.error('Error fetching product details:', error));
-}
-// Function to dynamically create and update the card elements
-function displayProducts(products) {
-    const resultContent = document.getElementById('result-content');
+  if (filter == "GoodDeal") {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => updateCard(data, filter))
+      .catch((error) =>
+        console.error("Error fetching product details:", error)
+      );
+  }
+};
 
-    // Clear existing content
-    resultContent.innerHTML = '';
+/**
+ * Function to dynamically create and update the card elements for GoodDeal
+ * */
+function displayProductsFakeStore(products) {
+  const resultContent = document.getElementById("result-content");
 
-    // Loop through each product and create a card
-    products.forEach(product => {
-        const card = document.createElement('div');
-        card.className = 'col-md-4';
+  // Clear existing content
+  resultContent.innerHTML = "";
 
-        // Card structure
-        // <div class="card" onclick="redirectToRetailer('${product.retailerUrl}', '${product.productId}')"></div>
-        card.innerHTML = `
+  // Loop through each product and create a card
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "col-md-4";
+
+    // Card structure
+    // <div class="card" onclick="redirectToRetailer('${product.retailerUrl}', '${product.productId}')"></div>
+    card.innerHTML = `
                     <div class="card">
-                        <img src="${product.images}" alt="Product Image">
+
+                        <img src="${product.image}" alt="Product Image">
                         <div class="card-body">
                             <h5 class="card-title">${product.title}</h5>
                             <p class="card-text">${product.description}</p>
-                            <p class="card-text">Price: $ ${product.price.toFixed(2)}</p>
+                            <p class="card-text">Price: $ ${product.price.toFixed(
+                              2
+                            )}</p>
                         </div>
                     </div>
                 `;
 
-        resultContent.appendChild(card);
-    });
+    resultContent.appendChild(card);
+  });
+}
+
+/**
+ * Function to dynamically create and update the card elements for Platzi
+ * */
+function displayProductsPlatzi(products) {
+  const resultContent = document.getElementById("result-content");
+
+  // Clear existing content
+  resultContent.innerHTML = "";
+
+  // Loop through each product and create a card
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.className = "col-md-4";
+
+    // Card structure
+    // <div class="card" onclick="redirectToRetailer('${product.retailerUrl}', '${product.productId}')"></div>
+    card.innerHTML = `
+                    <div class="card">
+
+                        <img src="${product.images}" alt="Product Image">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.title}</h5>
+                            <p class="card-text">${product.description}</p>
+                            <p class="card-text">Price: $ ${product.price.toFixed(
+                              2
+                            )}</p>
+                        </div>
+                    </div>
+                `;
+
+    resultContent.appendChild(card);
+  });
 }
 
 getFilter();
-
-// Fetch and display products when the page loads
-// window.onload = fetchAndDisplayProducts;
